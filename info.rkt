5a9dbedde0264b88f957cc0f9557cf67e7b25669
Leandro Facchinetti <me@leafac.com>
2017-02-10 10:04:31 -0500

    Add missing dependency

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "extensible-functions")
 (define version "0.0.1")
 (define deps '("base" "rackunit-lib" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc"))
 (define scribblings '(("documentation/extensible-functions.scrbl" ())))
 (define compile-omit-paths '("tests"))
 (define pkg-desc "A solution to the expression problem in Typed Racket.")
