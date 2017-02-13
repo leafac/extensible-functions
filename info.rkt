e8fcf852d607a4e6c644abbfb303aa4477369f45
Leandro Facchinetti <me@leafac.com>
2017-02-13 08:32:23 -0500

    Remove unnecessary directive

--- info.rkt
+++ info.rkt
@@ -5,6 +5,5 @@
 (define deps '("base" "rackunit-lib" "typed-racket-lib" "typed-racket-more"))
 (define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc"))
 (define scribblings '(("documentation/extensible-functions.scrbl" ())))
-(define compile-omit-paths '("tests"))
 (define pkg-desc "A solution to the expression problem in Typed Racket.")
 (define pkg-authors '(leafac))
